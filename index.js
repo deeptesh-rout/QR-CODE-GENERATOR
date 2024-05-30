a9e2f2b847ceffc9038e171d39e6b46511316fb7
deeptesh-rout <153610471+deeptesh-rout@users.noreply.github.com>
2024-05-30 11:08:50 +0530

    ok

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ qrText.addEventListener("input", handleQRText);
 sizes.addEventListener("change", handleSize);
 shareBtn.addEventListener("click", handleShare);
 
-const defaultUrl = "https://youtube.com/@AsmrProg";
+const defaultUrl = "https://github.com/deeptesh-rout?tab=repositories";
 let colorLight = "#fff",
     colorDark = "#000",
     text = defaultUrl,
